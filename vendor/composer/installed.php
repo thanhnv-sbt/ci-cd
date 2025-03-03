71ddb93290e3712f581b7427010bf552a91bc461
thanhnv-sbt <thanhnv@sabitech.vn>
2025-03-03 16:37:47 +0700

    add spell check

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '78f689aea72bf6710dc2fb0dc90ca91da9c933fc',
+        'reference' => '7c54b72511518a47e63c1d4820ded5512ccba155',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -424,7 +424,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '78f689aea72bf6710dc2fb0dc90ca91da9c933fc',
+            'reference' => '7c54b72511518a47e63c1d4820ded5512ccba155',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -1099,6 +1099,15 @@
             'replaced' => array(
                 0 => '*',
             ),
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.11.3',
+            'version' => '3.11.3.0',
+            'reference' => 'ba05f990e79cbe69b9f35c8c1ac8dca7eecc3a10',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
         ),
         'staabm/side-effects-detector' => array(
             'pretty_version' => '1.0.5',

929bd1c0709e487f8b880d7cbc236fceee90dfdc
thanhnv-sbt <thanhnv@sabitech.vn>
2025-03-03 17:00:04 +0700

    fix cicd pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,6 +1,6 @@
 <?php
 
-/* phpcs:disable Generic.Files.SideEffectsDeclaration */
+// phpcs:disable PSR1.Files.SideEffects
 
 /*
 |--------------------------------------------------------------------------
